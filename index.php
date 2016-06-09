d7d617c378500805ed0b050799e053cc0823e8c8
Chaosward <marten.ward@gmail.com>
2016-06-09 23:03:50 +0200

    Project Blog 0.0
    Nothing done yet…

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Modern Website Design</title>
+</head>
+<body>
+	<div id="header">
+		<img src="ubuntu.png">
+		<ul>
+			<li>Home</li>
+			<li>Styles</li>
+			<li>Categories</li>
+		</ul>
+	</div>
+</body>
+</html>
