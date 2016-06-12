2c30af0db1e275637be70297e12651e9616019c8
Chaosward <marten.ward@gmail.com>
2016-06-12 12:10:24 +0200

    Blog Site 0.1
    Header: Finished.
    Content: working.
    Sidebar: working.
    Page: N/A.
    Post: N/A.
    Footer: Finished.

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<title><?php bloginfo('title'); ?></title>
+</head>
+<body>
+	<div id="header">
+		<img src="<?php bloginfo('template_url'); ?>/photos/Header-Text.png">
+		<ul>
+			<li>Home</li>
+			<li>Styles</li>
+			<li>Categories</li>
+		</ul>
+	</div>
--- index.php
+++ index.php
@@ -1,17 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Modern Website Design</title>
-</head>
-<body>
-	<div id="header">
-		<img src="ubuntu.png">
+	<?php get_header();?>
+	<div class="content">
+		<h1>Welcome to my blog!</h1>
+		<p>
+		It is slightly shitty at the moment <br>
+		but do not worry! <br>
+		I have got the situation under control! <br> 
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <br>
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <br>
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo <br>
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <br> 
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non <br>
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
+		</p>
+	</div>
+	<div class="sidebar">
 		<ul>
-			<li>Home</li>
-			<li>Styles</li>
-			<li>Categories</li>
+			<li>Hi</li>
+			<li>I</li>
+			<li>Am</li>
+			<li>Bob</li>
+			<li>And</li>
+			<li>I</li>
+			<li>Like</li>
+			<li>You</li>
 		</ul>
+	</div>
+	<div id="footer">
+		<p>Ward&Laos Design</p>
+		<p>2016</p>
 	</div>
 </body>
 </html>
